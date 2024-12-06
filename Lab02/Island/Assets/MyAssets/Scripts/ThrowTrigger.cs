d790ab90bb6712f38bca048e5f8578277cd65371
xKerelek <136730573+xKerelek@users.noreply.github.com>
2024-12-06 09:40:53 +0100

    Wygranie baterii w rzucie kokosem

--- Lab02/Island/Assets/MyAssets/Scripts/ThrowTrigger.cs
+++ Lab02/Island/Assets/MyAssets/Scripts/ThrowTrigger.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThrowTrigger : MonoBehaviour
 {
+    public RawImage crosshair;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,7 @@ public class ThrowTrigger : MonoBehaviour
         if (col.gameObject.tag == "Player")
         {
             CoconutThrower.canThrow = true;
+            crosshair.enabled = true;
         }
     }
     void OnTriggerExit(Collider col)
@@ -28,6 +31,7 @@ public class ThrowTrigger : MonoBehaviour
         if (col.gameObject.tag == "Player")
         {
             CoconutThrower.canThrow = false;
+            crosshair.enabled=false;
         }
     }
 }
