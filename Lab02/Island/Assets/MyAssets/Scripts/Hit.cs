aa6c1033333939bc58720ebf6265afd25692c1cf
xKerelek <136730573+xKerelek@users.noreply.github.com>
2024-12-22 19:58:43 +0100

    GameOver po dotknięciu wilka

--- Lab02/Island/Assets/MyAssets/Scripts/Hit.cs
+++ Lab02/Island/Assets/MyAssets/Scripts/Hit.cs
@@ -1,29 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Hit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public RawImage gameOverPanel;
+    public Text restartText;
+    private bool isGameOver;
+
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        gameOverPanel.enabled = false;
+        isGameOver = false;
     }
 
     void OnCollisionEnter(Collision theObject)
     {
-        Debug.Log("Kolizja wykryta z obiektem: " + theObject.gameObject.name);
-
         if (theObject.gameObject.name == "coconut")
         {
             Debug.Log("Wilk zosta³ trafiony kokosem!");
             GetComponent<Animator>().SetTrigger("hit");
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Wilk zjad³ gracza!");
+            isGameOver = true;
+            gameOverPanel.enabled = true;
+            restartText.SendMessage("ShowHint", "Kliknij spacjê aby rozpocz¹æ od nowa lub kliknij myszk¹ na ekran");
+        }
+    }
+
+    void Update()
+    {
+        if (isGameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                LoadMenu();
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                LoadMenu();
+            }
+        }
+    }
+
+    void LoadMenu()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("Menu");
     }
 }
